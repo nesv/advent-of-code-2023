71f53c6d9f9d06545b4580eccb5428ec2fb31051
Nick Saika <n@nesv.ca>
2023-12-01 23:45:42 -0500

    Day 1 complete

--- src/01.zig
+++ src/01.zig
@@ -1,46 +1,112 @@
 const std = @import("std");
 const fmt = std.fmt;
 const mem = std.mem;
-const expect = std.testing.expect;
 const aoc = @import("root.zig");
 
 pub fn main() !void {
-    const stdin_file = std.io.getStdIn();
-    var bw = std.io.bufferedReader(stdin_file.reader());
-    const stdin = bw.reader();
-
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer {
-        const check = gpa.deinit();
-        if (check == .leak) expect(false) catch @panic("DEINIT FAILED");
+        const result = gpa.deinit();
+        if (result == .leak) std.testing.expect(false) catch @panic("LEAK DETECTED");
     }
     const alloc = gpa.allocator();
 
-    var list = std.ArrayList(usize).init(alloc);
-    defer list.deinit();
+    // Input for part 1.
+    var part1 = std.ArrayList(u8).init(alloc);
+    defer part1.deinit();
 
-    var sum: usize = 0;
-    var buffer: [100]u8 = undefined;
+    // Input for part 2.
+    var part2 = std.ArrayList(u8).init(alloc);
+    defer part2.deinit();
+
+    // Read from STDIN.
+    const stdin = std.io.getStdIn().reader();
+    var buffer: [1024]u8 = undefined;
     while (try aoc.readLine(stdin, &buffer)) |line| {
-        // If we encounter a blank line, take the current sum, and add it to
-        // our ArrayList.
-        if (line.len == 0) {
-            try list.append(sum);
-            sum = 0;
-            continue;
+        // Part 1.
+        {
+            const first = try fmt.charToDigit(firstDigitChar(line), 10);
+            const last = try fmt.charToDigit(lastDigitChar(line), 10);
+            const n = (first * 10) + last;
+            try part1.append(n);
         }
 
-        const n = try fmt.parseUnsigned(usize, line, 10);
-        sum += n;
+        // Part 2.
+        {
+            const first = firstDigit(line);
+            const last = lastDigit(line);
+            const n = (first * 10) + last;
+            try part2.append(n);
+        }
     }
 
-    // Find the elf carrying the most calories.
-    // const calories = try list.toOwnedSlice();
-    mem.sort(usize, list.items, {}, std.sort.desc(usize));
-    const max = list.items[0];
-    std.debug.print("{d}\n", .{max});
+    // Part 1 and part 2 involve summing all of the collected values together.
+    // The only difference is in how each line of input is parsed.
+    const p1sum = aoc.sum(u8, part1.items);
+    const p2sum = aoc.sum(u8, part2.items);
+    std.debug.print("{d}\n{d}\n", .{ p1sum, p2sum });
+}
 
-    // Part 2.
-    const top_three = list.items[0] + list.items[1] + list.items[2];
-    std.debug.print("{d}\n", .{top_three});
+fn firstDigitChar(line: []const u8) u8 {
+    for (line) |c| {
+        if (isDigit(c)) return c;
+    }
+    unreachable;
+}
+
+inline fn isDigit(c: u8) bool {
+    return (c >= '0' and c <= '9');
+}
+
+fn lastDigitChar(line: []const u8) u8 {
+    var i = line.len - 1;
+    while (i >= 0) : (i -= 1) {
+        if (isDigit(line[i])) return line[i];
+    }
+    unreachable;
+}
+
+const DigitName = struct {
+    name: []const u8,
+    value: u8,
+};
+
+const digits_by_name = [_]DigitName{
+    .{ .name = "one", .value = 1 },
+    .{ .name = "two", .value = 2 },
+    .{ .name = "three", .value = 3 },
+    .{ .name = "four", .value = 4 },
+    .{ .name = "five", .value = 5 },
+    .{ .name = "six", .value = 6 },
+    .{ .name = "seven", .value = 7 },
+    .{ .name = "eight", .value = 8 },
+    .{ .name = "nine", .value = 9 },
+    .{ .name = "1", .value = 1 },
+    .{ .name = "2", .value = 2 },
+    .{ .name = "3", .value = 3 },
+    .{ .name = "4", .value = 4 },
+    .{ .name = "5", .value = 5 },
+    .{ .name = "6", .value = 6 },
+    .{ .name = "7", .value = 7 },
+    .{ .name = "8", .value = 8 },
+    .{ .name = "9", .value = 9 },
+};
+
+fn firstDigit(line: []const u8) u8 {
+    for (0..line.len) |start| {
+        for (digits_by_name) |prefix| {
+            if (mem.startsWith(u8, line[start..], prefix.name)) return prefix.value;
+        }
+    }
+    unreachable;
+}
+
+fn lastDigit(line: []const u8) u8 {
+    var end: usize = line.len;
+    while (end > 0) : (end -= 1) {
+        for (digits_by_name) |suffix| {
+            if (mem.endsWith(u8, line[0..end], suffix.name)) return suffix.value;
+        }
+    }
+    unreachable;
 }
--- src/root.zig
+++ src/root.zig
@@ -1,21 +1,11 @@
 const std = @import("std");
-const fmt = std.fmt;
-const testing = std.testing;
-
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
-}
 
 /// Read a line of input from `reader`, where each line is denoted by a "\n"
 /// character.
 ///
 /// This was copied (and adapted) from
-/// https://ziglearn.org/chapter-2/#readers-and-writers, and it will also
-/// strip the trailing "\r" character from the end of the line.
+/// https://ziglearn.org/chapter-2/#readers-and-writers, and it will also strip
+/// the trailing "\r" character from the end of the line.
 pub fn readLine(reader: anytype, buffer: []u8) !?[]const u8 {
     const line = (try reader.readUntilDelimiterOrEof(buffer, '\n')) orelse return null;
 
@@ -26,19 +16,12 @@ pub fn readLine(reader: anytype, buffer: []u8) !?[]const u8 {
     return line;
 }
 
-/// Reads a line of input from `reader`, converting it to a signed or unsigned
-/// integer `T`, with the given `base`.
-///
-/// If the line that was read is empty -- because sometimes, the puzzles like
-/// to use blank lines as delimiters -- this function will return null.
-pub fn readInt(comptime T: type, reader: anytype, base: u8) !?T {
-    var buf: [100]u8 = undefined;
-    const line = (try readLine(reader, &buf)) orelse return null;
-    const n: T = (try fmt.parseInt(T, line, base)) orelse return null;
+/// Calculate the sum of all numeric values in `items`.
+pub fn sum(comptime T: type, items: []const T) isize {
+    var n: isize = 0;
+    for (items) |i| {
+        const m: isize = @intCast(i);
+        n += m;
+    }
     return n;
-}
-
-test "parseUnsigned" {
-    const n = try fmt.parseUnsigned(usize, "15931", 10);
-    try testing.expect(n == 15931);
 }
